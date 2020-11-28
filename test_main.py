99cc37e9c758b34125d132a2b103688d48f9b257
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-28 10:47:28 +0000

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,13 @@
+import unittest
+from main import *
+
+class UnitTests(unittest.TestCase) :
+    def test_graph(self) :
+        fighand=plt.gca()
+        figdat = fighand.get_lines()[0].get_xydata()
+        this_x, this_y = zip(*figdat)
+        self.assertTrue( len(this_x)==51, "The number of plotted data points is incorrect." )
+        for i in range( len(this_x) ) :
+            self.assertTrue( np.abs(i-this_x[i])<1e-7, "One or more of your x values are not correct" )
+            self.assertTrue( np.abs(0.1*i-this_y[i])<1e-7, "One or more of your y values are not correct" )
+            
